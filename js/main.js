31b5390e65ddab4a2b69a08013397c4d527e9092
Artur Sep <Artur Sep>
2016-04-06 04:56:34 +0200

    it is working

--- js/main.js
+++ js/main.js
@@ -1,44 +1,45 @@
 //============================================== ver.0.0  =========================================
-
 var apiKey = "3aa29487f83e54bcc224d7ad20d3fc8e";
-var lat, lon, city;
-
-jQuery.ajax({
-    url: '//freegeoip.net/json/',
-    type: 'POST',
-    dataType: 'jsonp',
-    success: function (location) {
-        // example where I update content on the page.
-        jQuery('#city').html(location.city);
-        jQuery('#region-code').html(location.region_code);
-        jQuery('#region-name').html(location.region_name);
-        jQuery('#areacode').html(location.areacode);
-        jQuery('#ip').html(location.ip);
-        jQuery('#zipcode').html(location.zipcode);
-        jQuery('#longitude').html(location.longitude);
-        jQuery('#latitude').html(location.latitude);
-        jQuery('#country-name').html(location.country_name);
-        jQuery('#country-code').html(location.country_code);
-
-        lat = location.latitude;
-        lon = location.longitude;
-        city = location.city;
-    }
-});
-
 
 function getWeather() {
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=" + apiKey, function (data) {
-        console.log(data);
+    var output = document.getElementById("out");
 
-        // var text = encodeURIComponent(data.quoteText);
+    if (!navigator.geolocation) {
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
 
-        $('#my-location').text(data.coord.lon + " --- " + data.coord.lat);
-        $('#my-temp').text(data.main.temp);
-        $('#my-icon').attr('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-        $('#city').text(data.name);
+    function success(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
 
-    });
+        output.innerHTML = '<p>Have a Nice Day :)</p>';
+
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=" + apiKey, function (data) {
+            console.log(data);
+
+            // var text = encodeURIComponent(data.quoteText);
+            // $('#my-location').text(data.coord.lat + " --- " + data.coord.lon);
+
+            $('#my-temp').text(data.main.temp);
+            $('#my-icon').attr('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+            $('#city').text(data.name);
+
+        });
+
+
+        // var img = new Image();
+        // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=8&size=200x100&sensor=false";
+        // output.appendChild(img);
+    };
+
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+    };
+
+    output.innerHTML = "<p>Locating…</p>";
+
+    navigator.geolocation.getCurrentPosition(success, error);
 }
 
 $(document).ready(function () {
